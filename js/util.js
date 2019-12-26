adbd166ca8704ea28c6aa8b2292b059fb4066b20
Snoop Zhang <2472305432@qq.com>
2019-12-26 15:27:54 +0800

    Update util.js

--- js/util.js
+++ js/util.js
@@ -153,3 +153,25 @@ const deepClone = obj => {
 
   return result
 }
+
+
+/**
+ * 产生随机整数，包含下限值，但不包括上限值
+ * @param {Number} lower 下限
+ * @param {Number} upper 上限
+ * @return {Number} 返回在下限到上限之间的一个随机整数
+ */
+function random(lower, upper) {
+	return Math.floor(Math.random() * (upper - lower)) + lower;
+}
+
+
+/**
+ * 产生随机整数，包含下限值，包括上限值
+ * @param {Number} lower 下限
+ * @param {Number} upper 上限
+ * @return {Number} 返回在下限到上限之间的一个随机整数
+ */
+function random(lower, upper) {
+	return Math.floor(Math.random() * (upper - lower+1)) + lower;
+}
